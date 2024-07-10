df61b9a397451a36d7cda7cd2f6f43b7c5f472c5
Ak8388 <akbarraw09@gmail.com>
2024-07-10 18:29:29 +0700

    add[unit test for update bidder]

--- src/test/java/jawa/sinaukoding/sk/service/AuctionTest.java
+++ src/test/java/jawa/sinaukoding/sk/service/AuctionTest.java
@@ -1,7 +1,9 @@
 package jawa.sinaukoding.sk.service;
 import java.math.BigInteger;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,19 +18,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Assertions;
 
 import jawa.sinaukoding.sk.repository.AuctionRepo;
 import jawa.sinaukoding.sk.repository.UserRepository;
+import jawa.sinaukoding.sk.entity.Auction;
 import jawa.sinaukoding.sk.entity.User;
 import jawa.sinaukoding.sk.model.Authentication;
 import jawa.sinaukoding.sk.model.Response;
 import jawa.sinaukoding.sk.model.request.SellerCreateAuctionReq;
+import jawa.sinaukoding.sk.model.request.UpdateHightBidReq;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AuctionTest {
     private static final User ADMIN = new User(1L, //
+            "ADMIN", //
+            "ADMIN@EXAMPLE.com", //
+            "PASSWORD", //
+            User.Role.ADMIN, //
+            0L, //
+            null, //
+            null, //
+            OffsetDateTime.now(), //
+            null, //
+            null); //
+
+            private static final User SALLER = new User(3L, //
             "ADMIN", //
             "ADMIN@EXAMPLE.com", //
             "PASSWORD", //
@@ -52,12 +69,13 @@ public class AuctionTest {
     @BeforeEach
     void findAdmin() {
         Mockito.when(userRepository.findById(ArgumentMatchers.eq(1L))).thenReturn(Optional.of(ADMIN));
+        Mockito.when(userRepository.findById(ArgumentMatchers.eq(3L))).thenReturn(Optional.of(SALLER));
         Mockito.when(userRepository.findById(ArgumentMatchers.eq(2L))).thenReturn(Optional.of(new User( //
                 2L, //
                 "Charlie", //
                 "charlie@example.com", //
                 "alice", //
-                User.Role.SELLER, //
+                User.Role.BUYER, //
                 ADMIN.id(), //
                 null, //
                 null, //
@@ -70,7 +88,7 @@ public class AuctionTest {
     @Test
     public void AuctionCreateFailed() {
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("jual barang orang", "saya menjual barang orang dengan harga start mulai dari 70000 saja", BigInteger.valueOf(10000), BigInteger.valueOf(200), "2025-07-10T14:32:45.123+07:00", "2025-07-10T17:20:45.123+07:00");
-        final User admin = userRepository.findById(1L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         Response<Object> response = auctionService.auctionCreate(authentication, req);
 
@@ -81,7 +99,7 @@ public class AuctionTest {
     @Test
     public void AuctionCreateFiledStartAtAfterEnd(){
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("jual barang orang", "saya menjual barang orang dengan harga start mulai dari 70000 saja", BigInteger.valueOf(10000), BigInteger.valueOf(200000), "2025-07-12T14:32:45.123+07:00", "2025-07-10T17:20:45.123+07:00");
-        final User admin = userRepository.findById(1L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         Response<Object> response = auctionService.auctionCreate(authentication, req);
 
@@ -91,7 +109,7 @@ public class AuctionTest {
 
     public void AuctionCreateFiledStartAtBeforeTimeNow(){
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("jual barang orang", "saya menjual barang orang dengan harga start mulai dari 70000 saja", BigInteger.valueOf(10000), BigInteger.valueOf(20000), "2025-07-07T14:32:45.123+07:00", "2025-07-10T17:20:45.123+07:00");
-        final User admin = userRepository.findById(1L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         Response<Object> response = auctionService.auctionCreate(authentication, req);
 
@@ -102,7 +120,7 @@ public class AuctionTest {
     @Test
     public void AuctionCreateFailedOfferPricetooLow() {
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("jual barang orang", "saya menjual barang orang dengan harga start mulai dari 70000 saja", BigInteger.valueOf(900), BigInteger.valueOf(1000), "2025-07-10T14:32:45.123+07:00", "2025-07-10T17:20:45.123+07:00");
-        final User admin = userRepository.findById(1L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         Response<Object> response = auctionService.auctionCreate(authentication, req);
 
@@ -113,7 +131,7 @@ public class AuctionTest {
     @Test
     public void AuctionCreateFailedSaveAuction() {
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("jual barang orang", "saya menjual barang orang dengan harga start mulai dari 70000 saja", BigInteger.valueOf(90000), BigInteger.valueOf(100000), "2025-07-10T14:32:45.123+07:00", "2025-07-10T17:20:45.123+07:00");
-        final User admin = userRepository.findById(2L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         Mockito.when(auctionRepo.saveAuction(ArgumentMatchers.any())).thenReturn(0L);
         Response<Object> response = auctionService.auctionCreate(authentication, req);
@@ -129,7 +147,7 @@ public class AuctionTest {
         BigInteger maximumPrice = new BigInteger("9000000");
         final SellerCreateAuctionReq req = new SellerCreateAuctionReq("ini bajuku dah","ini baju gufron legenda kita semuah",minimumPrice,maximumPrice,"2025-07-10T14:32:45.123+02:00","2025-07-11T14:32:45.123+02:00");
         Mockito.when(auctionRepo.saveAuction(ArgumentMatchers.any())).thenReturn(2L);
-        final User admin = userRepository.findById(1L).orElseThrow();
+        final User admin = userRepository.findById(3L).orElseThrow();
         final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
         
         Response<Object> response = auctionService.auctionCreate(authentication, req);
@@ -140,4 +158,318 @@ public class AuctionTest {
         Assertions.assertEquals(2L,response.data());
     }
     
+    @Test
+    public void updateHigestBidAndInsertBidTableSuccessTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null);
+    
+        Optional<Auction> optAuct = Optional.of(auction);
+
+        final UpdateHightBidReq req = new UpdateHightBidReq(auction.id(),BigInteger.valueOf(100000000));
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuct);
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+        Mockito.when(auctionRepo.updateHigestBidAndInsertBidTable(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(2L);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0107", response.code());
+        Assertions.assertEquals("sukses bid lelang", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedTest(){
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(null);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(100000000));
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+        Mockito.when(auctionRepo.updateHigestBidAndInsertBidTable(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(2L);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0206", response.code());
+        Assertions.assertEquals("auction is empty", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedNotApproveSttsTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.WAITING_FOR_APPROVAL, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null);
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(100000000));
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0103", response.code());
+        Assertions.assertEquals("tidak bisa bid kepada barang yang belum atau tidak di approve", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedNotYetStartedTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-19T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null);
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(100000000));
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0206", response.code());
+        Assertions.assertEquals("lelang belum di mulai atau sudah selesai", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedHigstBidStartedTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null);
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(1000));
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0306", response.code());
+        Assertions.assertEquals("highest bid request must be her", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedUserEmptyTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null);
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(10000000));
+
+        Mockito.when(userRepository.findById(ArgumentMatchers.any())).thenReturn(null);
+
+        final Authentication authentication = new Authentication(2L, User.Role.BUYER, true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0107", response.code());
+        Assertions.assertEquals("user is empty", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedUserHasBenDeletedTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null
+            );
+
+        User user = new User(null, null, null, null, null, null, null, null, null, null, OffsetDateTime.now(ZoneOffset.UTC));
+
+        Optional<Auction> optAuc = Optional.of(auction);
+        Optional<User> optUser = Optional.of(user);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(10000000));
+
+        Mockito.when(userRepository.findById(ArgumentMatchers.any())).thenReturn(optUser);
+
+        Mockito.when(auctionRepo.updateHigestBidAndInsertBidTable(ArgumentMatchers.any(),ArgumentMatchers.any())).thenReturn(1L);
+
+        final Authentication authentication = new Authentication(2L, User.Role.BUYER, true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0405", response.code());
+        Assertions.assertEquals("user sudah di hapus", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedRepo(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            null
+            );
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(10000000));
+
+        final User saller = userRepository.findById(2L).orElseThrow();
+
+        Mockito.when(auctionRepo.updateHigestBidAndInsertBidTable(ArgumentMatchers.any(),ArgumentMatchers.any())).thenReturn(0L);
+
+        final Authentication authentication = new Authentication(saller.id(), saller.role(), true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0302", response.code());
+        Assertions.assertEquals("gagal update auction", response.message());        
+    }
+
+    @Test
+    public void updateHigestBidAndInsertBidTableFailedAuctionHasBenDeletedTest(){
+        Auction auction = new Auction(
+            2L, 
+            UUID.randomUUID().toString().substring(0,8).toUpperCase(),
+            "Dukron", 
+            "Dukron di adol gah cah ikih ceg ana",
+            BigInteger.valueOf(30000), 
+            BigInteger.valueOf(300000), 
+            1L, 
+            "Dukron", 
+            Auction.Status.APPROVED, 
+            OffsetDateTime.parse("2024-07-09T14:32:45.123+07:00"), 
+            OffsetDateTime.parse("2025-07-12T14:32:45.123+07:00"), 
+            2L, 
+            null, 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC), 
+            null, 
+            OffsetDateTime.now(ZoneOffset.UTC));
+
+        Optional<Auction> optAuc = Optional.of(auction);
+
+        Mockito.when(auctionRepo.findById(ArgumentMatchers.any())).thenReturn(optAuc);
+        final UpdateHightBidReq req = new UpdateHightBidReq(1L,BigInteger.valueOf(1000000));
+
+        final User admin = userRepository.findById(2L).orElseThrow();
+
+        final Authentication authentication = new Authentication(admin.id(), admin.role(), true);
+
+        Response<Object> response = auctionService.updateHigestBidAndInsertBidTable(authentication, req);
+
+        Assertions.assertEquals("0301", response.code());
+        Assertions.assertEquals("bad request", response.message());        
+    }
+
 }
